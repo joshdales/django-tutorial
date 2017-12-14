a617d5c30f4c89339fda6d5d23a4a883383957e8
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-14 14:34:11 -0500

    Create Choice class in models

--- polls/models.py
+++ polls/models.py
@@ -4,3 +4,9 @@ from django.db import models
 class Question(models.Model):
     question_text = model.CharField(max_length=200)
     pub_date = model.DateTimeField("date published")
+
+class Choice(models.Model):
+    question = models.ForigenKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
