c3bb63c0f89aeac5a665908256e4be97e709dedf
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-14 14:30:47 -0500

    Create Questions class in models

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question_text = model.CharField(max_length=200)
+    pub_date = model.DateTimeField("date published")
