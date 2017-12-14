d2871c394a759913fb70f9c1a4bb568b5a0fe1c4
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-14 16:30:34 -0500

    Create vote in polls views

--- polls/views.py
+++ polls/views.py
@@ -9,3 +9,6 @@ def detail(request, question_id):
 
 def results(request, question_id):
     return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s" % question_id)
