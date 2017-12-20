11c7c6f53fda7076c2869dfd6c01193b63825c5c
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-20 20:05:20 +0000

    Change vote view to make post request

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
-from .models import Question
+
+from .models import Choice, Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -31,11 +33,13 @@ def vote(request, question_id):
     # return HttpResponse("You're voting on question %s" % question_id)
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = guestion.choice_set.get(pk=request.POST['choice'])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # redisplay yhe voting form
-        return render(request, 'polls/detail.html', {'question': question, 'error_message': "You didn't select a choice."})
+        # redisplay the voting form
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."})
     else:
-        selected_choice += 1
+        selected_choice.votes += 1
         selected_choice.save()
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
