c6449cbe823bfc80df7c0975346ff1e04cf1833f
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-12-22 16:18:47 +0000

    Choices can be added inline to the Question

--- polls/admin.py
+++ polls/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     # fields = ['pub_date', 'question_text']
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']})
     ]
-admin.site.register(Choice)
+    inlines = [ChoiceInline]
+# admin.site.register(Choice)
 admin.site.register(Question, QuestionAdmin)
